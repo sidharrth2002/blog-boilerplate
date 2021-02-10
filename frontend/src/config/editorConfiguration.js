3cb740ea41ede7f146c1749704182c92d58489bc
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-02-10 18:57:19 +0800

    separated frontend and backend

--- frontend/src/config/editorConfiguration.js
+++
@@ -1,7 +0,0 @@
-const editorConfiguration = {
-    // plugins: [ImagePlugin],
-    // plugins: [EasyImage],
-    toolbar: [ 'heading', '|', 'bold', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'MathType', 'image', 'imageUpload']
-};
-
-export default editorConfiguration;
